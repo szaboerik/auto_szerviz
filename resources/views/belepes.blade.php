b316794b3c09bf7ab301be9a99b20dec43b5fc77
Dombai Norbert <dombai.norbert@omikron.hu>
2022-03-21 15:38:14 +0100

    login felület css(not responsive)

--- resources/views/belepes.blade.php
+++ resources/views/belepes.blade.php
@@ -4,11 +4,32 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="{{ asset('css/stilus.css') }}" >
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/loginstilus.css') }}" >
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"/>
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
     <title>Bejelentkezés</title>
 </head>
-<body>
-    <form action="belepes" method="post">
+<body> 
+    <div class="container">
+        <div class="loginmezo">
+            <div class="title"><span>Bejelentkezés</span></div>
+            <form action="belepes" method="post">
+            @csrf
+                <div class="row">
+                    <i class="fas fa-user"></i>
+                    <input type="text" placeholder="Felhasználónév" name="fhn" id="fhn" required>
+                </div>
+                <div class="row">
+                     <i class="fas fa-lock"></i>
+                    <input type="password" placeholder="Jelszó" name="jsz" id="jsz" required>
+                </div>
+                <div class="row button">
+                    <input type="submit" value="Belépés" name="mehet">
+                </div>
+            </form>
+        </div>
+    </div>
+    <!--<form action="belepes" method="post">
     @csrf
     <div class="bejelentkezes">
     <form class="bejel">
@@ -18,7 +39,6 @@
         <input type="password" placeholder="Írd be a jelszabad" name="jsz" id="jsz" > <br>
     <input type="submit" value="Bejelentkezés" name="mehet"><br>
       </form>
-    </div>
-</form>
+    </div> -->
 </body>
 </html>
