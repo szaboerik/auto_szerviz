fc617e6c3439083bf000bdfd202a6512edc08306
Dombai Norbert <dombai.norbert@omikron.hu>
2022-04-14 10:01:05 +0200

    css táblázatok

--- resources/views/layouts/oldalmenu.blade.php
+++ resources/views/layouts/oldalmenu.blade.php
@@ -1,18 +1,5 @@
-<aside class="sidebar">
-            <header>
-                <div class="image-text">
-                        <span class="image" >
-                        <!--<img src="{{ asset('css/logo.png') }}" alt="logo">
-                            --></span>
-                  
-            <div class="text header-text">
-                        <span class="name">Racse Autószerelő Műhely</span>
-
-                </div>
-
-                </div>
-                <i class='bx bxs-chevron-right toggle'></i>
-            </hearder>
+<aside class="sidebar" id="sidebar">
+                
                 <div class="menubar">
                 <div class="menu">
                      
@@ -79,31 +66,16 @@
                             <span class="text nav-text">Munkalapok</span>
                         </a>
                     </li>
-
-                                                         
-                </ul>
-                </div>
-
-                <div class="bottom-content">
-                        <li>
+                    <li>
                             <a href="/kilepes">
                             <i class='bx bx-log-out icon' ></i>
                                 <span class="text nav-text">Kijelentkezés</span>
                             </a>
-                        </li>  
-
-                <li class="mode">
-                    <div class="moon-sun">
-                        <i class='bx bx-moon icon moon' ></i>
-                        <i class='bx bx-sun icon sun' ></i>
-                        <span class="mode-text text">Sötét téma</span>
-                      
-                    </div>
-                    <div class="toggle-switch">
-                        <span class="switch"></span>
-                    </div>
-                </li>       
+                        </li>      
                 
-                </div>   
+
+                                                         
+                </ul>
+                </div> 
             </div>
         </aside>
